75b9903f2a20cb90deae56f8d014ee826fcbc982
Mirkhalikov <42945722+Mirkhalikov@users.noreply.github.com>
2023-04-18 15:10:58 +0300

    Update Turing_machine.py

--- Turing_machine.py
+++ Turing_machine.py
@@ -3,14 +3,17 @@ if __name__ == "__main__":
     # Q = int(input("Введите кол-во состояний: "))
     m = int(input("Введите кол-во строк с описанием машины: "))
     print(f"Введите {m} cтрок с описанием машины. Пожалуйста, организуйте ввод в следующем порядке:\n"
-          f"Текущее состояние, входной символ, новое состояние, выходной символ, смещение, через пробел, без запятых")
+          f"Текущее состояние, входной символ, новое состояние, выходной символ, смещение - через пробел, без запятых")
 
     states = dict()  # name: dict(in_symb -> new state out_symb, shift)
+    A = set() # Алфавит
     for _ in range(m):
         # Считываем строку до тех пор, пока не введут правильно
         while True:
             try:
                 first_state, in_symb, new_state, out_symb, shift = input().split()
+                A.add(in_symb)
+                A.add(out_symb)
                 if shift not in ["+1", "-1"]:
                     print("Неверно введенная строка, попробуйте еще раз: ")
                     raise ValueError
@@ -27,9 +30,6 @@ if __name__ == "__main__":
             except ValueError:
                 pass
 
-    '''if len(states) != Q:
-        print("Неверное кол-во состояний!")
-        raise SystemExit'''
 
     tape = input("Введите входные данные на ленте (в качестве пустого символа используйте ^): ")
     n = int(input("Введите длину ленты: "))
@@ -42,10 +42,19 @@ if __name__ == "__main__":
     pos = 0
     cur_state = input("Введите название начального состояния: ")
     end_state = input("Введите название состояния, которое отвечает за окончание работы")
-    print(states)
+    maximum_iters = n * len(states) * len(A) ** n
+    iteration = 0
     while True:
+        iteration += 1
         if cur_state == end_state:
             print("OK")
             print(''.join(tape))
-            exit()
-        tape[pos], pos, cur_state = states[cur_state][tape[pos]][1], (pos + int(states[cur_state][tape[pos]][2])) % n, states[cur_state][tape[pos]][0]
+            break
+        if iteration > maximum_iters:
+            print("Not OK")
+            break
+        try:
+            tape[pos], pos, cur_state = states[cur_state][tape[pos]][1], (pos + int(states[cur_state][tape[pos]][2])) % n, states[cur_state][tape[pos]][0]
+        except KeyError:
+            print("Состояние машины, в котором она сейчас находится, не умеет обрабатывать данный символ")
+            break
